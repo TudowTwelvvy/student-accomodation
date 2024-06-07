81f06a6e4d91cb3ea8bf2e0ed740e4ab509eb0f5
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-06-07 11:49:50 +0200

    created the private route and profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import logo from '../assets/uni-accomodations-high-resolution-logo-transparent (1).png'
 import { BsSearch } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import {useSelector} from 'react-redux'
 
 function Header() {
+  const {currentUser} = useSelector(state=> state.user)
+
   return (
     <header>
       <div className='flex justify-between items-center mx-auto p-3'>
         <Link to='/'>
           <div className=''>
-            <img src={logo} alt="" className='w-[130px] md:w-[250px] '/>
+            <img src={logo} alt="" className='w-[130px] md:w-[250px]'/>
           </div>
         </Link>
         
@@ -22,7 +25,18 @@ function Header() {
         <ul className='flex justify-center items-center gap-2 lg:gap-4 text-sm md:text-base mr-2 lg:mr-7'>
           <Link to='/'><li className='hidden md:inline text-dark-blue hover:text-sec-blue cursor-pointer'>Home</li></Link>
           <Link to='/about'><li className='hidden md:inline text-dark-blue hover:text-sec-blue cursor-pointer'>About</li></Link>
-          <Link to='/sign-in'><li className='text-dark-blue hover:text-sec-blue cursor-pointer'>Sign in</li></Link>
+          <Link to='/profile'>
+          {
+            currentUser ? (
+              <img src={currentUser.avatar} alt=""  className='rounded-full h-7 w-7 object-fit'/>
+            ):(
+              
+                <li className='text-dark-blue hover:text-sec-blue cursor-pointer'>Sign in</li>
+              
+            )
+          }
+          </Link>
+          
         </ul>
       </div>
       
