c20b949c32bfdcc664ae0f74e4e40e1c3bb0d490
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-06-19 20:08:00 +0200

    completed sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -107,6 +107,22 @@ function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -137,7 +153,7 @@ function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-500 cursor-pointer' onClick={handleDeleteUser}>Delete Account</span>
-        <span className='text-red-500 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut} className='text-red-500 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-500 mt-5'>{error ? error : ""}</p>
       <p className='text-green-500 mt-5'>{updateSuccess? "User is updated successfully!": ""}</p>
