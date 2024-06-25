19c23f8c453cc6bdd903226cc4d983ee04d0a7fe
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-06-25 15:58:42 +0200

    added logos and completetd the home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-dark-blue'>About Uni-Accomodations</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Uni-Accommodation, your trusted companion in the search for the perfect place to live during your academic journey. We understand that where you live significantly impacts your university experience, and our mission is to simplify the process of finding suitable accommodation tailored to your needs.</p>
+      <p className='mb-4 text-slate-700'>
+      At Uni-Accommodation, we've curated a diverse range of listings that cater to students at every stage of their educational career. Whether you're a freshman looking for a lively dormitory environment or a graduate student seeking quiet, independent living, we have options that fit your lifestyle and budget.
+      </p>
+      <p className='mb-4 text-slate-700'>Our platform offers user-friendly tools and filters to streamline your search. Easily navigate through listings based on location, price range, amenities, and more, ensuring you find a residence that aligns perfectly with your preferences. Each listing on our site is carefully vetted to provide you with accurate information and peace of mind.</p>
+      <p className='mb-4 text-slate-700'>We prioritize transparency and convenience, offering detailed property descriptions, high-quality photos, and verified reviews from fellow students. Our goal is to empower you with the information you need to make informed decisions about where you'll live during your academic journey.</p>
+      <p className='mb-4 text-slate-700'>Join the thousands of students who have used Uni-Accommodation to discover their ideal living arrangements. Whether you're looking for a shared apartment close to campus or a private room with all the comforts of home, let us help you find your perfect fit. Begin your search today and embark on a seamless journey to finding your ideal student accommodation.</p>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,157 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import AccomodationItem from '../components/AccomodationItem';
+import ujLogo from '../assets/uj-logo.png'
+import cptLogo from '../assets/cpt logo.png'
+import nsfasLogo from '../assets/NSFAS-Status-Check-2024-Using-ID-Number.jpg'
+import studentsLogo from '../assets/istock-652270816.jpg'
+import vendaLogo from '../assets/venda.png'
+import nwuLogo from '../assets/nwu logo.png'
+
+const logosData= [ujLogo,nwuLogo,cptLogo,nsfasLogo,vendaLogo,nwuLogo]
 
 
-function Home() {
+
+export default function Home() {
+  const [nsfasAccomos, setNsfasAccomos] = useState([]);
+  const [cashPayingAccomos, setCashPayingAccomos] = useState([]);
+  const [otherBursaryAccomos, setOtherBursaryAccomos] = useState([]);
+  console.log(nsfasAccomos);
+
+
+ useEffect(() => {
+    const fetchNsfasAccomos = async () => {
+      try {
+        const res = await fetch('/api/accomodation/get?nsfas=true&limit=4');
+        const data = await res.json();
+        setNsfasAccomos(data);
+        fetchCashPayingAccomos();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchCashPayingAccomos = async () => {
+      try {
+        const res = await fetch('/api/accomodation/get?cashPaying=true&limit=4');
+        const data = await res.json();
+        setCashPayingAccomos(data);
+        fetchOtherBursaryAccomos();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchOtherBursaryAccomos = async () => {
+      try {
+        const res = await fetch('/api/accomodation/get?otherBursary=true&limit=4');
+        const data = await res.json();
+        setOtherBursaryAccomos(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNsfasAccomos();
+  }, []);
+//className='flex flex-1 flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'
   return (
-    <div>Home</div>
-  )
-}
+    <div >
+      {/* top */}
+      <div className='flex flex-col'>
+        <div className='flex items-center h-full '>
+        <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+          <h1 className='text-dark-blue font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-pri-blue'>perfect</span>
+            <br />
+          place with ease
+          </h1>
+          <div className='text-gray-400 text-xs sm:text-sm'>
+          Uni-Accomodations is the best place to find your next perfect place to
+          stay while you fighting to be called Mr West one day
+            <br />
+          We have a wide range of accomodations for you to choose from.
+          </div>
+          <Link
+          to={'/search'}
+          className='text-xs sm:text-sm text-sec-blue font-bold hover:underline'
+          >
+          Let's get started...
+          </Link>
+        </div>
 
-export default Home
+         <div className=''>
+          <img src={studentsLogo} alt="students" className='rounded-lg '/>
+         </div>
+
+        </div>
+        
+
+        <div className='mt-1 md:mt-12'>
+             <Slide >
+                {
+                logosData.map((logo,index) => ( 
+                  <div key={index}  className='pt-3 w-full'>
+                    <div >
+                       <img src={logo} alt={logo} className='object-contain h-[100px] md:h-[150px] w-full' />
+                    </div>
+                  </div>
+                  ))
+                }
+              </Slide>
+        </div>
+        
+         
+
+      </div>
+
+      {/* listing results for offer, sale and rent */}
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {nsfasAccomos && nsfasAccomos.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-dark-blue'>Recent nsfas accomodations</h2>
+              <Link className='text-sm text-sec-blue hover:underline' to={'/search?nsfas=true'}>Show more accomodations for nsfas students</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {nsfasAccomos.map((accomodation) => (
+                <AccomodationItem accomodation={accomodation} key={accomodation._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {cashPayingAccomos && cashPayingAccomos.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-dark-blue'>Recent accomodations for cash paying</h2>
+              <Link className='text-sm text-sec-blue hover:underline' to={'/search?cashPaying=true'}>Show more accomodations for cash paying students</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {cashPayingAccomos.map((accomodation) => (
+                <AccomodationItem  accomodation={accomodation} key={accomodation._id} />
+               
+              ))}
+            </div>
+          </div>
+        )}
+        {otherBursaryAccomos && otherBursaryAccomos.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-dark-blue'>Recent accomodations for bursaries</h2>
+              <Link className='text-sm text-sec-blue  hover:underline' to={'/search?otherBursary=true'}>Show more accomodations for bursary students</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {otherBursaryAccomos.map((accomodation) => (
+                <AccomodationItem  accomodation={accomodation} key={accomodation._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <p className='bg-dark-blue text-white text-center mb-0'>coded by Tumelo (TwelvvyTudow)</p>
+    </div>
+  );
+}
